d1a18a2ab8b6ab1f4c2c20bebe07d2dae6cac895
Al Cheung <cangzan@gmail.com>
2020-03-03 22:37:08 +0800

    feat: add profile link to nav.

---
+++ add-profile-link-to-nav.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Add profile link to nav.
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Add profile link to nav bar.
+// @author       Al Cheung
+// @match        *://github.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const $ = document.querySelector.bind(document);
+    const headerList = $(`nav.d-flex`);
+    const a = document.createElement(`a`);
+    a.className = `js-selected-navigation-item Header-link  mr-3`;
+    a.innerText = `My Profile`;
+    const userName = $(`details > summary > img`).alt.replace(`@`, '');
+    a.href = `//github.com/${userName}`;
+    headerList.appendChild(a);
+})();
