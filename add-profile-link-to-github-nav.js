f18f0b83a41aafecbc8d22313e396ef996a45e73
Al Cheung <cangzan@gmail.com>
2020-04-23 18:08:09 +0800

    style(github link): fixed middle issue.

--- add-profile-link-to-github-nav.js
+++ add-profile-link-to-github-nav.js
@@ -14,7 +14,7 @@
     const $ = document.querySelector.bind(document);
     const headerList = $(`nav.d-flex`);
     const a = document.createElement(`a`);
-    a.className = `js-selected-navigation-item Header-link  mr-3`;
+    a.className = `js-selected-navigation-item Header-link py-lg-3  mr-0 mr-lg-3 py-2 border-top border-lg-top-0 border-white-fade-15`;
     a.innerText = `My Profile`;
     const userName = $(`details > summary > img`).alt.replace(`@`, '');
     a.href = `//github.com/${userName}`;
