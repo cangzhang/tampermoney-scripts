d682e799714932a55403a4f25a3d472c7f45163f
al <cangzan@gmail.com>
2020-09-17 10:14:23 +0800

    style(github): button style.

--- add-profile-link-to-github-nav.js
+++ add-profile-link-to-github-nav.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github.com: Add profile link to nav.
 // @namespace    https://greasyfork.org/zh-CN/scripts/397263
-// @version      0.3
+// @version      0.4
 // @description  Add profile link to nav bar.
 // @author       Al Cheung
 // @match        *://github.com/*
@@ -14,7 +14,7 @@
     const $ = document.querySelector.bind(document);
     const headerList = $(`nav.d-flex`);
     const a = document.createElement(`a`);
-    a.className = `js-selected-navigation-item Header-link py-lg-3  mr-0 mr-lg-3 py-2 border-top border-lg-top-0 border-white-fade-15`;
+    a.className = `js-selected-navigation-item Header-link mt-md-n3 mb-md-n3 py-2 py-md-3 mr-0 mr-md-3 border-top border-md-top-0 border-white-fade-15`;
     a.innerText = `My Profile`;
     const userName = $(`details > summary > img`).alt.replace(`@`, '');
     a.href = `//github.com/${userName}`;
