e52dbd9050753513e3dbc82be32f27d2434562fb
al <cangzan@gmail.com>
2021-07-20 10:54:18 +0800

    fix: clear task correctly.

--- remove-modals.coding-net.js
+++ remove-modals.coding-net.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove fucking modals 移除 coding.net 强制弹窗
 // @namespace    https://github.com/cangzhang/tampermoney-scripts
-// @version      0.1
+// @version      0.2
 // @description  remove all modals that are showed when visiting coding.net
 // @author       alcheung
 // @match        *://*.coding.com/*
@@ -23,6 +23,8 @@
       });
       removed = true;
       console.info(`[tampermonkey: Remove fucking modals] 弹窗已移除`);
+    }
+    if (removed || !modals.length) {
       clearInterval(handle);
     }
   }, 1500);
